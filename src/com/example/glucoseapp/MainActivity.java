7f84875bc33bf1dfb4adaa6858cfb68159bd928c
shehaaz <shehaaz@gmail.com>
2013-07-14 16:30:23 -0400

    Added Drop Down menu with Food items

--- src/com/example/glucoseapp/MainActivity.java
+++ src/com/example/glucoseapp/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.glucoseapp;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -20,20 +26,25 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ArrayList<String> food_list = new ArrayList<String>();
 		
+		/*Loading the Data from CSV File*/
+
 		InputStream is = getResources().openRawResource(R.raw.gldata);
-		
+
 		try {
 			BufferedReader brReadMe = new BufferedReader
 					(new InputStreamReader(is, "UTF-8"));
-			
+
 			String strLine;
 
 			while ((strLine = brReadMe.readLine()) != null){
 
 				String[] RowData = strLine.split(",");
 				String food = RowData[1];
-				System.out.println(food);		
+				food_list.add(food);
+
 			} 
 
 			brReadMe.close();
@@ -49,9 +60,13 @@ public class MainActivity extends Activity {
 			e.printStackTrace();
 		}
 
+		/*Auto-complete TextView*/
+		
+		AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autocomplete_food);
 
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,food_list);
 
-
+		textView.setAdapter(adapter);
 	}
 
 	@Override
@@ -60,5 +75,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
