ce2f5bda556b41492cae9ae8a16008e644da8747
shehaaz <shehaaz@gmail.com>
2013-07-14 15:19:46 -0400

    initialized csv parsing

--- src/com/example/glucoseapp/MainActivity.java
+++ src/com/example/glucoseapp/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.glucoseapp;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
 
 public class MainActivity extends Activity {
 
@@ -10,6 +20,38 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		InputStream is = getResources().openRawResource(R.raw.gldata);
+		
+		try {
+			BufferedReader brReadMe = new BufferedReader
+					(new InputStreamReader(is, "UTF-8"));
+			
+			String strLine;
+
+			while ((strLine = brReadMe.readLine()) != null){
+
+				String[] RowData = strLine.split(",");
+				String food = RowData[1];
+				System.out.println(food);		
+			} 
+
+			brReadMe.close();
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+
 	}
 
 	@Override
