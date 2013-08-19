19a68d6dc9d6a155a22af9aee091998a90c07639
shehaaz <shehaaz@gmail.com>
2013-08-19 15:18:45 -0400

    Update FoodItemListAdapter.java

--- src/com/example/glucoseapp/FoodItemListAdapter.java
+++ src/com/example/glucoseapp/FoodItemListAdapter.java
@@ -31,36 +31,52 @@ public class FoodItemListAdapter extends ArrayAdapter<FoodItem> {
 		View row = convertView;
 		ItemHolder holder = null;
 		
-		FoodItem food_item = food_items.get(position);
-		
+	
 		if(row == null){
 			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			row = inflater.inflate(layoutResourceId, parent, false);
 			
 			holder = new ItemHolder();
 			
+			holder.food_item = food_items.get(position);
 			holder.food_name = (TextView) row.findViewById(R.id.list_food_item_name);
 			holder.food_item_servings = (TextView) row.findViewById(R.id.list_food_item_servings);
 			holder.food_amount_servings = (EditText) row.findViewById(R.id.list_amount_of_servings);
+			holder.send_button = (ImageView) row.findViewById(R.id.confirm_button);
 			
 			row.setTag(holder);
+			holder.send_button(holder);
 		}
 		else{
 			holder = (ItemHolder) row.getTag();
 		}
 		
-		holder.food_name.setText(food_item.getFoodName());
-		holder.food_item_servings.setText("Grams per servings: "+ food_item.getServingSizeGrams());
-//		food_item.setMealServing(holder.food_amount_servings.getText().toString());
+		holder.food_name.setText(holder.food_item.getFoodName());
+		holder.food_item_servings.setText("Grams per servings: "+ holder.food_item.getServingSizeGrams());
+//		
+		holder.send_button.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View v){
+				ItemHolder tempHolder = (ItemHolder) v.getTag();
+				
+				String serving_amount = tempHolder.food_amount_servings.getText().toString();
+				holder.food_item.setMealServing(serving_amount);
+				
+			}
+			
+			});
 		
 		return row;
 		
 	}
 	
 	static class ItemHolder{
+		FoodItem food_item;
 		TextView food_name;
 		TextView food_item_servings;
 		EditText food_amount_servings;
+		ImageView send_button;
 	}
 
 }
